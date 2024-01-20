e3def5e396cb55485422bf62e449023a66e40135
Tocinofrito <chichamalo@gmail.com>
2024-01-20 15:45:22 -0600

    configurada base de datos

--- first microservice/my-new-project/azure-storage/src/index.js
+++ first microservice/my-new-project/azure-storage/src/index.js
@@ -3,10 +3,69 @@ const azure = require('azure-storage'); //Lo que usaremos para contectar con azu
 
 const app = express();
 
-const PORT = process.env.PORT; //Tomamos el puerto de la variable de entorno ya puesta en el dockerfile
-const STORAGE_ACCOUNT_NAME = process.env.STORAGE_ACCOUNT_NAME; //La tomamos de la variable de entorno
-const STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY;//Igual de la env
-
-function createBlobService(){
-  
+if (!process.env.PORT) {
+  throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
+
+if (!process.env.STORAGE_ACCOUNT_NAME) {
+  throw new Error("Please specify the name of an Azure storage account in environment variable STORAGE_ACCOUNT_NAME.");
+}
+
+if (!process.env.STORAGE_ACCESS_KEY) {
+  throw new Error("Please specify the access key to an Azure storage account in environment variable STORAGE_ACCESS_KEY.");
+}
+const PORT = process.env.PORT; //Tomamos el puerto de la variable de entorno ya puesta en el dockerfile
+//Descomentamos para pruebas
+//const PORT = 3000
+const STORAGE_ACCOUNT_NAME = process.env.STORAGE_ACCOUNT_NAME; //La tomamos de la variable de entorno
+//Descomentamos para pruebas
+//const STORAGE_ACCOUNT_NAME ="microservicios";
+const STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY;//Igual de la env
+//Descomentamos para pruebas
+//const STORAGE_ACCESS_KEY="JfjcZZXjPCqoF97fkPPmW1f8CcNnKH4hasK2FdCQk+1Jq4qNJubKWTVqqveCw/qrwN3CyFQ6MnTd+AStGBHqGg==";
+
+console.log(`Serving videos from Azure storage account ${STORAGE_ACCOUNT_NAME}.`);
+
+function createBlobService() {
+  const blobService = azure.createBlobService(STORAGE_ACCOUNT_NAME, STORAGE_ACCESS_KEY);
+  // Uncomment next line for extra debug logging.
+  //blobService.logger.level = azure.Logger.LogLevels.DEBUG; 
+  return blobService;
+}
+
+app.get("/video", (req, res) =>{
+  const videoPath = req.query.path;
+  console.log(`aqui debe ir el nombre de ruta${videoPath}`)
+  console.log(`Streaming video from path ${videoPath}.`);
+  const blobService = createBlobService();
+
+  const containerName = "videos"; //Este debemos cambiar esto para cada usuario
+  //Obtenemos las propiedades de videos de azure storage
+  blobService.getBlobProperties(containerName, videoPath, (err, properties) =>{
+    if (err){
+      //Manejo de error
+      console.error(`Error occurred getting properties for video ${containerName}/${videoPath}.`);
+      console.error(err && err.stack || err);
+      res.sendStatus(500);
+      return;
+    }
+    //En caso de no haber error
+    res.writeHead(200, {
+      "Content-Length" : properties.contentLength,
+      "Content-Type": "video/mp4",
+    });
+      //Aquí obtenemos el video en forma de stream para mostrarlo en el micro
+      //Servicio principal
+    blobService.getBlobToStream(containerName, videoPath, res, err =>{
+      if(err){
+        //Manejamos el error
+        res.sendStatus(500);
+        return;
+      }
+    });
+  });
+});
+
+app.listen(PORT, () =>{
+  console.log('Microservicio azure-storage online');
+})
--- first microservice/my-new-project/video-streaming/src/index.js
+++ first microservice/my-new-project/video-streaming/src/index.js
@@ -1,44 +1,125 @@
 const express = require('express');
-const fs = require("fs");
-const path = require('path');
+//Usaremos esta libreria para peticion http al otro microservicio
+const http = require("http");
+//const fs = require("fs"); Ya no necesitamos estas 2 ya que usaremos peticiones http para los demás microservicios
+//const path = require('path');
+//Traemos la libreria con la que trabajaremos con mongodb
+const mongodb = require("mongodb");
 
 const app = express();
+//
+// Manda error si cualquier de las env variables no existe
+//
 
-//Requerimos la variable de entorno, si no se recibe se envía error
-if(!process.env.PORT){
-  throw new Error("Porfavor especifica el numero de puerto para el server http con la variable de entorno Port.");
+if (!process.env.PORT) {
+  throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
-//Así recibimos el puerto:
+if (!process.env.VIDEO_STORAGE_HOST) {
+  throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+  throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+}
+
+if (!process.env.DBHOST) {
+  throw new Error("Please specify the databse host using environment variable DBHOST.");
+}
+
+if (!process.env.DBNAME) {
+  throw new Error("Please specify the name of the database using environment variable DBNAME");
+}
+
+//Así recibimos el puerto como variable de entorno:
 const port = process.env.PORT;
 //const port = 3000;
+//Configuramos la conección al microservicio video-storage
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+//Traemos las env variables para el dbhost y dbname
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-app.get('/', (req, res) =>{
-  res.send('Hola mundo');
-  console.log('Holiwis')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hola mundo');
+//   console.log('Holiwis')
+// })
+function main() {
+  //Conectamos a la db
+  return mongodb.MongoClient.connect(DBHOST)
+    .then(client => {
+      //recuperamos la database que usa el microservicio usa
+      const db = client.db(DBNAME);
+      //Recuperamos la colección de videos donde almacenamos los metadatos de cada una
+      const videosCollection = db.collection("videos");
+      app.get('/video', (req, res) => {
+        //especificamos el id de video por un parametro http
+        const videoId = new mongodb.ObjectId(req.query.id);
+        //Hace query a la db para traer el de id requerido
+        videosCollection.findOne({ _id: videoId })
+          .then(videoRecord => {
+            if (!videoRecord) {
+              res.sendStatus(404);
+              return;
+            }
 
-app.get('/video', (req, res) => {
-  const videoPath = path.join('./videos', 'SampleVideo_1280x720_1mb.mp4');
-  fs.stat(videoPath, (err, stats) => {
-    if (err) {
-      console.error('Un error ocurrió');
-      res.sendStatus(500);
-      return;
-    }
+            const forwartRequest = http.request(//El primer parametro es la conexión al microservicio
+              {
+                host: VIDEO_STORAGE_HOST,
+                port: VIDEO_STORAGE_PORT,
+                //Cuando seguimos la request http a video-storage service mapeamos el id del video con su localizacion
+                path: `/video?path=${videoRecord.videoPath}`,
+                method: 'GET',
+                headers: req.headers
+              },
+              //El callback al recibir la respuesta al GET
+              forwardResponse => {//Ya lo habíamos trabajado, para mandar el stream
+                res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+                forwardResponse.pipe(res);
+              }
+            );
 
-    // Mandamos cabecera al navegador para reconocer el contenido a enviar
-    res.writeHead(200, {
-      'Content-Length': stats.size,
-      'Content-Type': 'video/mp4',
+
+            req.pipe(forwartRequest);
+          })
+          .catch(err => {
+            console.error("database query failed.");
+            console.error(err && err.stack || err);
+            res.sendStatus(500);
+          });
+
+        //Lo siguiente comentado es la manera en como traiamos el video y lo mostrabamos
+        //Antes del microservicio que nos traerá esa información/archivos
+        // const videoPath = path.join('./videos', 'SampleVideo_1280x720_1mb.mp4');
+        // fs.stat(videoPath, (err, stats) => {
+        //   if (err) {
+        //     console.error('Un error ocurrió');
+        //     res.sendStatus(500);
+        //     return;
+        //   }
+
+        //   // Mandamos cabecera al navegador para reconocer el contenido a enviar
+        //   res.writeHead(200, {
+        //     'Content-Length': stats.size,
+        //     'Content-Type': 'video/mp4',
+        //   });
+
+        //   // Utilizamos createReadStream con la ruta corregida
+        //   fs.createReadStream(videoPath).pipe(res);
+        // });
+      });
+
+
+      app.listen(port, () => {
+        console.log(`Microservicio principal en puerto: ${port}`);
+      });
     });
+}
 
-    // Utilizamos createReadStream con la ruta corregida
-    fs.createReadStream(videoPath).pipe(res);
-  });
-});
-
-
-app.listen(port, () =>{
-  console.log(`Escuchando en puerto ${port}`);
-})
+main()
+  .then(() => console.log("Microservicio online"))
+  .catch(err => {
+    console.error("Microservicio falló el iniciar");
+    console.error(err && err.stack || err);
+  })
